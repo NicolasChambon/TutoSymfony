706e048bb0eadbf5b780057684df9635f221e1ce
NicolasChambon <nico.chambon@gmail.com>
2024-09-06 14:11:49 +0200

    end of chapter 7 - forms

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Recipe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSubmitEvent;
 use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,17 +20,20 @@ class RecipeType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('slug')
+            ->add('slug', TextType::class, [
+                'required' => false
+            ])
             ->add('content')
             ->add('duration')
             ->add('save', SubmitType::class, [
                 'label' => 'Envoyer'
             ])
             ->addEventListener(FormEvents::PRE_SUBMIT, $this->autoSlug(...))
+            ->addEventListener(FormEvents::POST_SUBMIT, $this->attachTimestamps(...))
         ;
     }
 
-    public function autoSlug(PreSubmitEvent $event)
+    public function autoSlug(PreSubmitEvent $event): void
     {
         $data = $event->getData();
         if (empty($data['slug'])) {
@@ -36,8 +41,22 @@ class RecipeType extends AbstractType
             $data['slug'] = $slugger->slug($data['title']);
             $event->setData($data);
         }
-
     }
+
+    public function attachTimestamps(PostSubmitEvent $event): void
+    {
+        $data = $event->getData();
+        if (!($data instanceof Recipe)) {
+            return;
+        }
+
+        $data->setUpdatedAt(new \DateTimeImmutable());
+        if (!$data->getId()) {
+            $data->setCreatedAt(new \DateTimeImmutable());
+        }
+    }
+
+        
 
     public function configureOptions(OptionsResolver $resolver): void
     {

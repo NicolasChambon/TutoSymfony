4468591627c8f659f2334f5efa9b1fbddef8b8f6
NicolasChambon <nico.chambon@gmail.com>
2024-09-18 20:32:56 +0200

    data validation chapter 8 done

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -7,11 +7,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Event\PostSubmitEvent;
 use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextAreaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\String\Slugger\AsciiSlugger;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
 
 
 class RecipeType extends AbstractType
@@ -19,11 +22,13 @@ class RecipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'empty_data' => ''])
             ->add('slug', TextType::class, [
-                'required' => false
+                'required' => false,
             ])
-            ->add('content')
+            ->add('content', TextAreaType::class, [
+                'empty_data' => ''])
             ->add('duration')
             ->add('save', SubmitType::class, [
                 'label' => 'Envoyer'
